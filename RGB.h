d046057048cf0827197d3034ae6b3d769f4e8eb7
John Lawrence <johnonrails@gmail.com>
2016-03-29 04:18:39 -0700

    Initial commit

---
+++ RGB.h
@@ -0,0 +1,50 @@
+#ifndef _RGB_H
+#define _RGB_H
+
+#include <Arduino.h>
+
+#define COMPONENT_MAX 255
+
+struct RGB {
+  uint8_t r;
+  uint8_t g;
+  uint8_t b;
+  RGB(unsigned long value) : r(value >> 16), g(value >> 8), b(value) {}
+  RGB(uint8_t _r, uint8_t _g, uint8_t _b) : r(_r), g(_g), b(_b) {}
+  RGB() : r(0), g(0), b(0) {}
+
+  inline RGB operator*(float multiplier) {
+    return RGB(r * multiplier, g * multiplier, b * multiplier);
+  }
+
+  inline RGB operator<<(RGB other) {
+    return RGB(max(r, other.r), max(g, other.g), max(b, other.b));
+  }
+
+  inline RGB operator+(RGB other) {
+    return RGB(min(r + other.r, COMPONENT_MAX), min(g + other.g, COMPONENT_MAX), min(b + other.b, COMPONENT_MAX));
+  }
+
+  RGB logged() {
+    return RGB(r * r / 256, g * g / 256, b * b / 256);
+  }
+
+  RGB adjusted() {
+    return RGB(r * r / 255, g * g / 255, b * b / 255);
+  }
+
+  RGB interpolate(RGB other, float m) {
+    float m_ = 1.0 - m;
+    return RGB(round(r * m_ + other.r * m), round(g * m_ + other.g * m), round(b * m_ + other.b * m));
+  }
+
+  void print() {
+    Serial.print(r);
+    Serial.print(", ");
+    Serial.print(g);
+    Serial.print(", ");
+    Serial.println(b);
+  }
+};
+
+#endif
